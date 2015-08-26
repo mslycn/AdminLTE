ece745f1fa51c49abe82a2a36bd5db53b6cea977
Jay Lucas <jayluke780@gmail.com>
2015-08-26 15:04:14 -0400

    react components

--- AdminLTE/src/AdminLTE/Startup.cs
+++ AdminLTE/src/AdminLTE/Startup.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Authentication.Facebook;
-using Microsoft.AspNet.Authentication.Google;
+﻿using Microsoft.AspNet.Authentication.Facebook;
 using Microsoft.AspNet.Authentication.MicrosoftAccount;
-using Microsoft.AspNet.Authentication.Twitter;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Routing;
 using Microsoft.Data.Entity;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
 using Microsoft.Framework.Runtime;
 using AdminLTE.Models;
 using AdminLTE.Services;
+
 
 namespace AdminLTE
 {
@@ -122,7 +113,7 @@ namespace AdminLTE
             // app.UseGoogleAuthentication();
             // app.UseMicrosoftAccountAuthentication();
             // app.UseTwitterAuthentication();
-
+             
             // Add MVC to the request pipeline.
             app.UseMvc(routes =>
             {
