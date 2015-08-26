b3888a066211355fd7e23228d5a0a3aba6673a86
Jay Lucas <jayluke780@gmail.com>
2015-08-26 11:49:00 -0400

    Updated master page and added registure template

--- AdminLTE/src/AdminLTE/wwwroot/_references.js
+++ AdminLTE/src/AdminLTE/wwwroot/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="dist/js/app.js" />
+/// <reference path="dist/js/app.min.js" />
 /// <reference path="dist/js/demo.js" />
 /// <reference path="dist/js/pages/dashboard.js" />
 /// <reference path="dist/js/pages/dashboard2.js" />
