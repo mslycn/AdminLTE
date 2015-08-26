96d894c5f76ed85c3554fe5cce1a0963e966356b
Jay Lucas <jayluke780@gmail.com>
2015-08-26 14:05:08 -0400

    adding reactjs components

--- AdminLTE/src/AdminLTE/wwwroot/_references.js
+++ AdminLTE/src/AdminLTE/wwwroot/_references.js
@@ -4,4 +4,3 @@
 /// <reference path="dist/js/demo.js" />
 /// <reference path="dist/js/pages/dashboard.js" />
 /// <reference path="dist/js/pages/dashboard2.js" />
-/// <reference path="js/site.js" />
