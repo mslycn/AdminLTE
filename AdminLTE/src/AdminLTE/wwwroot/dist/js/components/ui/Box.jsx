a52b14eb06826f9fa23d3700edc36c6a2bea927a
Jay Lucas <jayluke780@gmail.com>
2015-08-27 17:16:40 -0400

    First Reactjs Component "Box"

--- AdminLTE/src/AdminLTE/wwwroot/dist/js/components/ui/Box.jsx
+++ AdminLTE/src/AdminLTE/wwwroot/dist/js/components/ui/Box.jsx
@@ -1,24 +1,26 @@
 ﻿var Box = React.createClass({
+    displayName: 'Box',
     getInitialState: function () {
         return {title: this.props.title}
     },
     render: function () {
         return (
-                    <div className="box">
-                        <div className="box-header with-border">
-                            <h3 className="box-title">{this.state.title}</h3>
-                            <div className="box-tools pull-right">
-                                <button className="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i className="fa fa-minus"></i></button>
-                                <button className="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i className="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                        <div className="box-body">
-                            {this.props.children}
-                        </div>
-                        <div className="box-footer">
-                            Footer
+
+                <div className="box">
+                    <div className="box-header with-border">
+                        <h3 className="box-title">{this.state.title}</h3>
+                        <div className="box-tools pull-right">
+                            <button className="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i className="fa fa-minus"></i></button>
+                            <button className="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i className="fa fa-times"></i></button>
                         </div>
                     </div>
+                    <div className="box-body">
+                        Start creating your amazing application!
+                    </div>
+                    <div className="box-footer">
+                        Footer
+                    </div>
+                </div>
                );
     }
 });
