6d9cab2f7dfcb506789fc32ec6c90db3860d0ceb
Jay Lucas <jayluke780@gmail.com>
2015-08-31 13:41:19 -0400

    Progressbar addition

---
+++ AdminLTE/src/AdminLTE/wwwroot/dist/js/components/widget/Progressbar.jsx
@@ -0,0 +1,34 @@
+﻿var Progressbar = React.createClass({
+    getInitialState: function () {
+        return { info: {} };
+    },
+    componentDidMount: function () {
+        var sec = this.props.interval;
+        var url = this.props.dataurl;
+        var self = this;
+        if (url != undefined) {
+            setInterval(function () {
+                $.ajax({
+                    url: url,
+                    dataType: 'json',
+                    success: function (data) {
+                        self.setState({ info: data });
+                    }.bind(this)
+                });
+            }, 5000);
+        }
+    },
+    displayName: 'Progressbar',
+    render: function () {
+        return (
+                    <div>
+                            <div className="progress">
+                                <div className="progress-bar" style={{width: this.state.info}}></div>
+                            </div>
+                            <span className="progress-description">
+                                {this.state.info}% {this.props.message}
+                            </span>
+                    </div>
+               );
+    }
+});
